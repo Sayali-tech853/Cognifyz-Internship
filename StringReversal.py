6e715998f2fed76dd19c95a1e3a894bcf9ad8da3
Sayali Shivpuje <115891685+Sayali-tech853@users.noreply.github.com>
2023-12-19 22:54:15 +0530

    Add files via upload

---
+++ StringReversal.py
@@ -0,0 +1,8 @@
+original_string = "Hello"
+
+# Reversed string
+reversed_string = original_string[::-1]
+
+# Displaying the result
+print("Original string:", original_string)
+print("Reversed string:", reversed_string)
